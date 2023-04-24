f23901bd5dd185c7be108f9e1820560c85e31c99
Anton Kuzmin <furthertwoant@gmail.com>
2023-04-24 22:34:38 +0400

    Configured text field

--- ColorizedSwiftUI/ContentView.swift
+++ ColorizedSwiftUI/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
     
+    @FocusState private var isFocused: Bool
+    
     private var viewColor: Color {
         Color(
             red: redSliderValue / 255,
@@ -21,21 +23,46 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(viewColor)
                 .frame(height: 200)
             
-            ColoredSliderView(value: $redSliderValue, color: .red)
-            ColoredSliderView(value: $greenSliderValue, color: .green)
-            ColoredSliderView(value: $blueSliderValue, color: .blue)
+            HStack {
+                VStack(spacing: 40) {
+                    ColoredSliderView(value: $redSliderValue, color: .red)
+                    ColoredSliderView(value: $greenSliderValue, color: .green)
+                    ColoredSliderView(value: $blueSliderValue, color: .blue)
+                }
+                
+                VStack(spacing: 40) {
+                    TextFieldView(value: $redSliderValue)
+                        .focused($isFocused)
+                    TextFieldView(value: $greenSliderValue)
+                        .focused($isFocused)
+                    TextFieldView(value: $blueSliderValue)
+                        .focused($isFocused)
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done", action: {
+                            isFocused = false
+                        })
+                    }
+                }
+            }
             
             Spacer()
+        }
+        .onTapGesture {
+            isFocused = false
         }
         .padding()
     }
 }
 
+// MARK: - Slider with value label
 struct ColoredSliderView: View {
     @Binding var value: Double
     
@@ -48,7 +75,19 @@ struct ColoredSliderView: View {
             Slider(value: $value, in: 0...255, step: 1)
                 .tint(color)
         }
-        .padding()
+    }
+}
+
+// MARK: - Text field
+struct TextFieldView: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        TextField("", value: $value, formatter: NumberFormatter())
+            .textFieldStyle(.roundedBorder)
+            .frame(width: 45, height: 30)
+            .multilineTextAlignment(.center)
+            .keyboardType(.numberPad)
     }
 }
 
