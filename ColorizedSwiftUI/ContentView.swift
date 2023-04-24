6dc65e477950910f0d93a14293ac3b6fcddc4ae4
Anton Kuzmin <furthertwoant@gmail.com>
2023-04-24 18:04:26 +0400

    Implemented color changing by slider

--- ColorizedSwiftUI/ContentView.swift
+++ ColorizedSwiftUI/ContentView.swift
@@ -8,18 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValues = Double.random(in: 0...255)
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    private var viewColor: Color {
+        Color(
+            red: redSliderValue / 255,
+            green: greenSliderValue / 255,
+            blue: blueSliderValue / 255
+        )
+    }
     
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 20).foregroundColor(.accentColor)
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(viewColor)
                 .frame(height: 200)
             
-            ColoredSliderView(color: .red)
-            ColoredSliderView(color: .green)
-            ColoredSliderView(color: .blue)
+            ColoredSliderView(value: $redSliderValue, color: .red)
+            ColoredSliderView(value: $greenSliderValue, color: .green)
+            ColoredSliderView(value: $blueSliderValue, color: .blue)
             
             Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ColoredSliderView: View {
+    @Binding var value: Double
+    
+    let color: Color
+    
+    var body: some View {
+        HStack() {
+            Text("\(lround(value))")
+                .frame(width: 45, height: 30)
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
         }
         .padding()
     }
